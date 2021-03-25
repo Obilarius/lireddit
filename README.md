cb90e69d3fa9a13b1f2ea185c32266ba9eaf037c
Obilarius <git@walzenbach.com>
2021-03-25 15:27:15 +0100

    2:27:00 - Own InputField

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # lireddit
+
+Based on the Tutorial from Ben Awad
+[Youtube Video](https://www.youtube.com/watch?v=I6ypD7qv3Z8)
--- web/src/components/InputField.tsx
+++ web/src/components/InputField.tsx
@@ -1,9 +1,28 @@
-export interface InputFieldProps {
-  
-}
- 
-const InputField: React.FC<InputFieldProps> = () => {
-  return (  );
-}
- 
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+} from "@chakra-ui/form-control";
+import { Input } from "@chakra-ui/input";
+import { useField } from "formik";
+import { InputHTMLAttributes } from "react";
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  label: string;
+  placeholder?: string;
+};
+
+const InputField: React.FC<InputFieldProps> = (props) => {
+  const [field, { error }] = useField(props);
+
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor="username">{props.label}</FormLabel>
+      <Input {...field} id={field.name} placeholder={props.placeholder} />
+      {error && <FormErrorMessage>{error}</FormErrorMessage>}
+    </FormControl>
+  );
+};
+
 export default InputField;
